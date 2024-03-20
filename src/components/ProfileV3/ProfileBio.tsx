b2231779abd50a897c96c598611fc90c2e36b8af
aish96 <aish.96jain@gmail.com>
2024-03-20 11:49:41 +0530

    feat: profile cards

---
+++ src/components/ProfileV3/ProfileBio.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ReactComponent as CopyIcon } from '../../assets/images/copy.svg'
+import { ReactComponent as TickIcon } from '../../assets/images/check.svg'
+
+interface User {
+  user: UserProps
+}
+
+interface UserProps {
+  name: string
+  avatar: string
+  userId: string
+  kleo: number
+}
+
+export default function ProfileBio({ user }: User) {
+  return (
+    <div className="flex flex-col self-stretch items-center justify-start">
+      <div className="flex flex-col justify-center items-center px-24 pt-6 pb-4">
+        <img
+          src={user.avatar}
+          className="w-24 h-24 rounded-full border-4 border-white"
+        />
+        <h3 className="text-2xl font-medium text-gray-900">{user.name}</h3>
+      </div>
+      <div className="max-w-sm text-center p-2 text-gray-800">
+        building @kleo_network, member of superteam DAO and passionate developer
+      </div>
+    </div>
+  )
+}
---
+++ src/components/profile/ProfileCards/TextCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface TextCardProps {
+  title?: string
+  subtitle?: string
+  content: string
+}
+
+const TextCard: React.FC<TextCardProps> = ({ title, subtitle, content }) => {
+  return (
+    <div className="flex-1 bg-gray-100 p-6 rounded-lg shadow-md relative">
+      {title && <h2 className="text-xl font-bold">{title}</h2>}{' '}
+      {subtitle && <h3 className="text-md text-gray-500">{subtitle}</h3>}
+      <p className="text-gray-700 mt-2">{content}</p>
+    </div>
+  )
+}
+
+export default TextCard
