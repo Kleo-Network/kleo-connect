0a400676feede03f935bd3c0c97abe9919765d52
BhishmaGohel <bhishmagohel09@gmail.com>
2024-04-25 19:34:18 +0530

    fix: public profile

--- src/components/ProfileV2/index.tsx
+++ src/components/ProfileV2/index.tsx
@@ -24,8 +24,8 @@ export default function ProfileV2({ user, setUser }: UserDataProps) {
   const { slug } = useParams()
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (token) {
+    const slug_from_local_storage = localStorage.getItem('slug')
+    if (slug_from_local_storage == slug) {
       setIsPublic(false)
     } else {
       setIsPublic(true)
@@ -98,7 +98,9 @@ export default function ProfileV2({ user, setUser }: UserDataProps) {
           if (data) {
             console.log(data)
             setUserFullData(data)
-            setUser(data.user)
+            if (!isPublic) {
+              setUser(data.user)
+            }
           }
         }
       })
