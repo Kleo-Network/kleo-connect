b6320aebbdea1b4af286e65c856df25ad48b261c
BhishmaGohel <bhishmagohel09@gmail.com>
2024-05-06 11:08:27 +0530

    fix: add pin icon by default

--- src/components/profile/ProfileCards/MapCard.tsx
+++ src/components/profile/ProfileCards/MapCard.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { MapCard as MapCardType } from '../../common/interface'
+import markerIconUrl from '../../../assets/images/marker-icon.png'
+import { icon } from 'leaflet'
+
+// Define the custom marker icon
+const customIcon = icon({
+  iconUrl: markerIconUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28]
+})
+
 interface MapCardProps {
   map: MapCardType
 }
@@ -19,7 +31,10 @@ const MapCard: React.FC<MapCardProps> = ({ map }) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[map.cordinates.lat, map.cordinates.lng]}>
+        <Marker
+          position={[map.cordinates.lat, map.cordinates.lng]}
+          icon={customIcon}
+        >
           <Popup>{map.location}</Popup>
         </Marker>
       </MapContainer>
