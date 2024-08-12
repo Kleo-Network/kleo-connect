df205a4bf2312c877b04e6635fe071b8245b5ea6
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-12 15:45:53 +0530

    fix: updated YTCardBody component

--- src/components/profile/Feed/FeedCardBody/YTCardBody.tsx
+++ src/components/profile/Feed/FeedCardBody/YTCardBody.tsx
@@ -11,58 +11,64 @@ interface ThumbnailProps {
 }
 
 export function YTCardBody({ card }: YTCardProps) {
+    const videoUrls: string[] = card.urls.map(url => extractThumbNailURL(url.url));
 
-    const videoUrls: string[] = [];
-
-    card.urls.forEach(url => {
-        videoUrls.push(extractThumbNailURL(url.url));
-    })
+    const renderThumbnails = () => {
+        switch (videoUrls.length) {
+            case 4:
+                return (
+                    <div className="flex flex-wrap justify-between items-center content-center w-full gap-2 h-full">
+                        <div className="flex items-center justify-center w-full grow">
+                            <Thumbnail url={videoUrls[0]} extraStyles="w-full" />
+                        </div>
+                        <div className="flex flex-row gap-2 grow">
+                            {videoUrls.slice(1).map((url, index) => (
+                                <Thumbnail key={index} url={url} />
+                            ))}
+                        </div>
+                    </div>
+                );
+            case 3:
+                return (
+                    <div className="flex flex-wrap justify-between items-center content-center w-full gap-2 h-full">
+                        <div className="flex items-center justify-center w-full grow">
+                            <Thumbnail url={videoUrls[0]} extraStyles="w-full grow" />
+                        </div>
+                        <div className="flex flex-row gap-2 grow">
+                            {videoUrls.slice(1).map((url, index) => (
+                                <Thumbnail key={index} url={url} />
+                            ))}
+                        </div>
+                    </div>
+                );
+            case 2:
+                return (
+                    <div className="flex flex-wrap xl:flex-col justify-evenly items-center content-center w-full gap-2 h-full">
+                        {videoUrls.map((url, index) => (
+                            <Thumbnail key={index} url={url} />
+                        ))}
+                    </div>
+                );
+            case 1:
+                return (
+                    <div className="flex flex-wrap justify-between items-center content-center w-full gap-2 h-full">
+                        <Thumbnail url={videoUrls[0]} />
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
 
     return (
         <div className="h-full w-full flex content-center">
-            {videoUrls.length === 4 && (
-                <div className="flex flex-wrap justify-between items-center content-center w-full gap-2 h-full">
-                    <div className="flex items-center justify-center w-full grow">
-                        <Thumbnail url={videoUrls[0]} extraStyles="w-full" />
-                    </div>
-                    <div className="flex flex-row gap-2 grow">
-                        <Thumbnail url={videoUrls[1]} extraStyles="grow" />
-                        <Thumbnail url={videoUrls[2]} extraStyles="grow" />
-                        <Thumbnail url={videoUrls[3]} extraStyles="grow" />
-                    </div>
-                </div>
-            )}
-
-            {videoUrls.length === 3 && (
-                <div className="flex flex-wrap justify-between items-center content-center w-full gap-2 h-full">
-                    <div className="flex items-center justify-center w-full grow">
-                        <Thumbnail url={videoUrls[0]} extraStyles="w-full" />
-                    </div>
-                    <div className="flex flex-row gap-2 grow">
-                        <Thumbnail url={videoUrls[1]} extraStyles="grow" />
-                        <Thumbnail url={videoUrls[2]} extraStyles="grow" />
-                    </div>
-                </div>
-            )}
-
-            {videoUrls.length === 2 && (
-                <div className="flex flex-wrap xl:flex-col justify-evenly items-center content-center w-full gap-2 h-full">
-                    <Thumbnail url={videoUrls[1]} extraStyles="grow" />
-                    <Thumbnail url={videoUrls[0]} extraStyles="grow" />
-                </div>
-            )}
-
-            {videoUrls.length === 1 && (
-                <div className="flex flex-wrap justify-between items-center content-center w-full gap-2 h-full">
-                    <Thumbnail url={videoUrls[0]} extraStyles="grow" />
-                </div>
-            )}
+            {renderThumbnails()}
         </div>
-    )
+    );
 }
 
 function Thumbnail({ url, extraStyles = '' }: ThumbnailProps) {
     return (
-        <img src={url} alt="Thumbnail" className={`bg-gray-200 rounded-lg object-cover min-w-[90px] aspect-[1.7] ${extraStyles}`} />
-    )
+        <img src={url} alt="Thumbnail" className={`bg-gray-200 rounded-lg object-cover grow min-w-[60px] aspect-[1.7] ${extraStyles}`} />
+    );
 }
